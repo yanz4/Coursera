5a6ddb737d7c681318e445eb955d5f4c46e0c0b2
Yan Zhang <yanz4@illinois.edu>
2018-08-26 15:53:32 -0500

    Initial commit

---
+++ Data Structure/Programming _Assignment_1_Basic_Data_Structures/Network packet processing simulation.py
@@ -0,0 +1,35 @@
+# python3
+import numpy as np
+import sys
+
+[S, n] = list(map(int, sys.stdin.readline().split()))
+a = []
+f = []
+for i in range(n):
+    a.append(list(map(int, sys.stdin.readline().split())))
+
+for i in a:
+
+    [A, p] = i
+    if len(f) == 0:
+        f.append(A + p)
+        print(A)
+    else:
+        u = 0
+        num = len(f)
+        while f[u] <= A:
+            u += 1
+            if u == num: break
+        f = f[u:]
+        '''f = [i for i in f if i > A]'''
+
+        if len(f) == S:
+            print(-1)
+        else:
+            if len(f) == 0:
+                f.append(A + p)
+                print(A)
+            else:
+                t = max(A, f[-1])
+                f.append(t + p)
+                print(t)
